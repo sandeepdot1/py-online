90cf09eb6106b32ff24aa6a1dc0fcafa25603237
Sandeep Kumar Panigrahi <sandeeppanigrahi3570@gmail.com>
2024-09-19 18:58:48 +0000

    Added singleton code snippet

---
+++ singleton.py
@@ -0,0 +1,23 @@
+import threading
+
+my_lock = threading.Lock()
+
+class SingleTon:
+    obj = None
+
+    def __new__(cls):
+        with my_lock:
+            if cls.obj is None:
+                cls.obj = super().__new__(cls)
+                return cls.obj
+        
+        return cls.obj
+    
+
+obj1 = SingleTon()
+obj2 = SingleTon()
+
+print(id(obj1) == id(obj2))
+
+    
+    
